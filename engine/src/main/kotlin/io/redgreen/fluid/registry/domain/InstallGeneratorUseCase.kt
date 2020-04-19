3eb4e274c9aa0eba43ca8cddc48dbc3800e0d37d
Ragunath Jawahar <ragunath@redgreen.io>
2020-04-19 22:38:28 +0530

    improvement: handle overwrites in install generator use case
    CRF-82

--- engine/src/main/kotlin/io/redgreen/fluid/registry/domain/InstallGeneratorUseCase.kt
+++ engine/src/main/kotlin/io/redgreen/fluid/registry/domain/InstallGeneratorUseCase.kt
@@ -3,9 +3,13 @@ package io.redgreen.fluid.registry.domain
 import com.squareup.moshi.Moshi
 import io.redgreen.fluid.engine.domain.ValidateGeneratorUseCase.Result.ValidGenerator
 import io.redgreen.fluid.registry.domain.CopyGeneratorUseCase.Result.GeneratorCopied
+import io.redgreen.fluid.registry.domain.InstallGeneratorUseCase.InstallationType.FRESH
+import io.redgreen.fluid.registry.domain.InstallGeneratorUseCase.InstallationType.OVERWRITE
 import io.redgreen.fluid.registry.domain.InstallGeneratorUseCase.Result.FreshInstallSuccessful
+import io.redgreen.fluid.registry.domain.InstallGeneratorUseCase.Result.OverwriteSuccessful
 import io.redgreen.fluid.registry.model.Registry
 import io.redgreen.fluid.registry.model.RegistryEntry
+import java.nio.file.Files
 import java.nio.file.Path
 
 class InstallGeneratorUseCase(
@@ -16,13 +20,27 @@ class InstallGeneratorUseCase(
     candidate: ValidGenerator,
     installationType: InstallationType
   ): Result {
+    if (installationType == OVERWRITE) {
+      deleteAlreadyInstalledArtifact(candidate.manifest.generator.id)
+    }
+
     return when (val result = CopyGeneratorUseCase(registry).invoke(candidate)) {
       is GeneratorCopied -> {
         val artifactName = getArtifactName(result.destinationPath)
         val generatorId = result.manifest.generator.id
         val entry = RegistryEntry(generatorId, artifactName)
-        AddRegistryEntryUseCase(registry, moshi).invoke(entry)
-        FreshInstallSuccessful(entry)
+
+        when (installationType) {
+          FRESH -> {
+            AddRegistryEntryUseCase(registry, moshi).invoke(entry)
+            FreshInstallSuccessful(entry)
+          }
+
+          OVERWRITE -> {
+            UpdateRegistryUseCase(registry, moshi).invoke(entry)
+            OverwriteSuccessful(entry)
+          }
+        }
       }
     }
   }
@@ -30,13 +48,25 @@ class InstallGeneratorUseCase(
   private fun getArtifactName(artifactPath: Path): String =
     artifactPath.fileName.toString()
 
+  private fun deleteAlreadyInstalledArtifact(generatorId: String) {
+    val existingEntryForGenerator = registry.getRegistryEntry(generatorId)
+    if (existingEntryForGenerator.isPresent) {
+      val alreadyInstalledArtifactPath = registry.artifactsPath.resolve(existingEntryForGenerator.get().artifactName)
+      Files.delete(alreadyInstalledArtifactPath)
+    }
+  }
+
   sealed class Result {
     data class FreshInstallSuccessful(
+      val registryEntry: RegistryEntry
+    ) : Result()
+
+    data class OverwriteSuccessful(
       val registryEntry: RegistryEntry
     ) : Result()
   }
 
   enum class InstallationType {
-    FRESH
+    FRESH, OVERWRITE
   }
 }
--- engine/src/test/kotlin/io/redgreen/fluid/registry/domain/InstallGeneratorUseCaseTest.kt
+++ engine/src/test/kotlin/io/redgreen/fluid/registry/domain/InstallGeneratorUseCaseTest.kt
@@ -7,12 +7,15 @@ import io.redgreen.fluid.engine.domain.ValidateGeneratorUseCase.Result.ValidGene
 import io.redgreen.fluid.registry.assist.ValidGeneratorParameterResolver
 import io.redgreen.fluid.registry.assist.ValidGeneratorParameterResolver.TestArtifact
 import io.redgreen.fluid.registry.domain.InstallGeneratorUseCase.InstallationType.FRESH
+import io.redgreen.fluid.registry.domain.InstallGeneratorUseCase.InstallationType.OVERWRITE
 import io.redgreen.fluid.registry.domain.InstallGeneratorUseCase.Result.FreshInstallSuccessful
+import io.redgreen.fluid.registry.domain.InstallGeneratorUseCase.Result.OverwriteSuccessful
 import io.redgreen.fluid.registry.model.Registry
 import io.redgreen.fluid.registry.model.RegistryEntry
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.io.TempDir
+import java.nio.file.Files
 import java.nio.file.Path
 
 @ExtendWith(ValidGeneratorParameterResolver::class)
@@ -20,24 +23,49 @@ class InstallGeneratorUseCaseTest {
   @TempDir
   internal lateinit var supposedlyUserHomeDir: Path
 
-  private val registry by lazy {
-    Registry.from(supposedlyUserHomeDir)
-  }
+  private val registry by lazy { Registry.from(supposedlyUserHomeDir) }
+  private val installGeneratorUseCase by lazy { InstallGeneratorUseCase(registry, moshi) }
 
   @Test
-  fun `it should add an entry to the registry after installation`(
-    @TestArtifact(ARTIFACT_VALID_GENERATOR) validGenerator: ValidGenerator
+  fun `it should add an entry to the registry after installation`( // FIXME This test is not verifying if a registry entry was added.
+    @TestArtifact(ARTIFACT_VALID_GENERATOR) candidate: ValidGenerator
   ) {
-    // given
-    val useCase = InstallGeneratorUseCase(registry, moshi)
-
     // when
-    val result = useCase.invoke(validGenerator, FRESH) as FreshInstallSuccessful
+    val result = installGeneratorUseCase.invoke(candidate, FRESH) as FreshInstallSuccessful
 
     // then
+    val freshInstallSuccessful = FreshInstallSuccessful(RegistryEntry("generator-id", ARTIFACT_VALID_GENERATOR))
     assertThat(result)
-      .isEqualTo(
-        FreshInstallSuccessful(RegistryEntry("generator-id", ARTIFACT_VALID_GENERATOR))
-      )
+      .isEqualTo(freshInstallSuccessful)
+    val installedArtifactExists = Files.exists(registry.artifactsPath.resolve(ARTIFACT_VALID_GENERATOR))
+    assertThat(installedArtifactExists)
+      .isTrue()
+  }
+
+  @Test // FIXME This test is not verifying if a registry entry was updated.
+  fun `it should update an entry in the registry after overwriting an already installed generator`(
+    @TestArtifact(ARTIFACT_VALID_GENERATOR) installed: ValidGenerator,
+    @TestArtifact("valid-generator-different-sha256.jar") candidate: ValidGenerator
+  ) {
+    // given
+    val freshInstallResult = installGeneratorUseCase.invoke(installed, FRESH)
+    assertThat(freshInstallResult)
+      .isEqualTo(FreshInstallSuccessful(RegistryEntry("generator-id", ARTIFACT_VALID_GENERATOR)))
+
+    // when
+    val result = installGeneratorUseCase.invoke(candidate, OVERWRITE)
+
+    // then
+    val overwriteSuccessful = OverwriteSuccessful(RegistryEntry("generator-id", "valid-generator-different-sha256.jar"))
+    assertThat(result)
+      .isEqualTo(overwriteSuccessful)
+
+    val installedArtifactDoesNotExist = Files.notExists(registry.artifactsPath.resolve(ARTIFACT_VALID_GENERATOR))
+    assertThat(installedArtifactDoesNotExist)
+      .isTrue()
+
+    val overwrittenArtifactExists = Files.exists(registry.artifactsPath.resolve("valid-generator-different-sha256.jar"))
+    assertThat(overwrittenArtifactExists)
+      .isTrue()
   }
 }
